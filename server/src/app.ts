cbf16b34a2e54a9c6146ca05dd1dd8fe28d386a1
Zion-Webiks <zion@webiks.com>
2024-12-01 11:37:17 +0200

    first commit

---
+++ server/src/app.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import usersController from "./controllers/users";
+import adminController from "./controllers/admin";
+import votesController from "./controllers/votes";
+import candidatesController from "./controllers/candidates";
+import connectToMongo from "./config/db";
+import http from "http";
+import { Server } from "socket.io";
+import { handleSocketConnection } from "./sockets/io";
+
+const PORT = process.env.PORT || 3000
+
+export const app = express();
+
+connectToMongo();
+const httpServer = http.createServer(app);
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
+
+io.on("connection", handleSocketConnection);
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", usersController);
+app.use("/api/admin", adminController);
+app.use("/api/votes", votesController);
+app.use("/api/candidates", candidatesController);
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send("pong");
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started, Visit "http://localhost:${PORT}"`);
+});
