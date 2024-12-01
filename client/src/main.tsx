3fd861e0bfffa8380b8d1674695e46a34358446b
Zion-Webiks <zion@webiks.com>
2024-12-01 14:24:22 +0200

    done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import store from "./redux/store.ts";
 import { BrowserRouter } from "react-router-dom";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3031//");
+export const socket = io("http://localhost:3031/");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
